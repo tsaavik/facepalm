3ea46bf78a597327ed49a3d4e3d255d239c91d6a
David Mcanulty <git @ hellspark.com>
2011-01-04 12:20:35 -0800

    changed external date call to internal perl call
    No reason to fire up a shell to run date to get epoch time, just me being lazy/dumb.

--- facepalm.pl
+++ facepalm.pl
@@ -3,8 +3,7 @@
 #use warnings;
 #
 print "Content-type: text/html\n\n";
-my $epoch=`date +%s`;
-chomp($epoch);
+my $epoch = time;
 my @file=`ls images/*.jpg`;
 my $lines = scalar(@file);  #get number of lines
 srand;     #seed random
@@ -12,6 +11,6 @@ my $random_number=rand @file;   #page 52, perl cookbook :)
 my $random_number = int($random_number);
 my $random_file=$file[$random_number];   #page 52, perl cookbook :)
 chomp($random_file);
-print "<img src=/images/$random_file?$epoch><br>\n";
+print "<img src=$random_file?$epoch><br>\n";
 $random_number++; #so we don't say 0 of 6
 print "$random_number of $lines<br>\n";
